7e03ba573eedb7ddb5d5ebb8abf70939ec8bc5ee
Pablo Coviza <vanityone@yahoo.es>
2023-10-07 09:26:41 +0200

    Ultimos cambios

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+function aboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Obcaecati eligendi dolorem rem excepturi saepe quaerat corrupti, 
+                ut facilis numquam ipsa cum est perferendis neque repellendus? 
+                Non labore nam illum consequatur!</p>
+        </div>
+    )
+}
+
+export default aboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,13 @@
+function servicesPage() {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Enim rem molestiae fugit incidunt, pariatur illo. Aspernatur, eius provident. 
+                Veritatis officiis doloribus velit doloremque assumenda sint 
+                sequi voluptatum blanditiis reprehenderit quibusdam.</p>
+        </div>
+    )
+}
+
+export default servicesPage
